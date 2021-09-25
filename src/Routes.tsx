11979944eed899fd58abe782511abc5e0164a3ac
karolis-vegvisir <karolis.blazevicius@vegvisir.lt>
2021-09-25 16:15:57 +0300

    Add Routes.

---
+++ src/Routes.tsx
@@ -0,0 +1,33 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import App from "./App";
+// import Game from "./Game";
+
+function Routes() {
+    return (
+        <div>
+            <Router>
+                <div>
+
+                    <Switch>
+                        <Route path="/app">
+                            <App />
+                        </Route>
+                        <Route path="/game">
+                            <App />
+                            {/* <Game /> */}
+                        </Route>
+                        <Route path="/">
+                            <App />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
+}
+
+export default Routes;
